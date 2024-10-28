883100f9c1cf7964a2b7e29140e8d54084ebffc7
Jonathan Hallstrom <lmj.hallstrom@gmail.com>
2024-10-28 11:49:20 +0100

    add version using simd in zig

--- src/main.zig
+++ src/main.zig
@@ -38,6 +38,49 @@ fn despaceStdlib(inp: []const u8, outp: []u8) []u8 {
 // size_t despace_avx512(const char *src, char *dst, size_t len)
 extern fn despace_avx512(src: [*]const u8, dst: [*]u8, len: usize) callconv(.C) usize;
 
+fn despaceZigSimd(inp: []const u8, outp: []u8) []u8 {
+    var out_i: usize = 0;
+    var inp_i: usize = 0;
+    const unroll = @sizeOf(usize);
+    const masks = blk: {
+        var res: [unroll]@Vector(unroll, bool) = undefined;
+        res[0] = @splat(true);
+        for (1..unroll) |i| {
+            res[i] = std.simd.shiftElementsRight(res[i - 1], 1, false);
+        }
+        break :blk res;
+    };
+    const MaskT = std.meta.Int(.unsigned, unroll);
+    while (inp_i + unroll - 1 < inp.len) : (inp_i += unroll) {
+        const chars: @Vector(unroll, u8) = inp[inp_i..][0..unroll].*;
+        var mask: MaskT = 0;
+        for (" \n\t") |delim| {
+            mask |= @bitCast(chars == @as(@Vector(unroll, u8), @splat(delim)));
+        }
+        const space_count = @popCount(mask);
+        if (space_count == 0) continue;
+        if (space_count == 1) {
+            const dist_to_space = @ctz(mask);
+            const shifted_mask = masks[dist_to_space];
+            const shifted_right = std.simd.shiftElementsLeft(chars, 1, 0);
+            const to_write: [unroll]u8 = @select(u8, shifted_mask, shifted_right, chars);
+            @memcpy(outp[out_i..][0..unroll], &to_write);
+            out_i += unroll - 1;
+        } else {
+            for (inp[inp_i..][0..unroll]) |ch| {
+                outp[out_i] = ch;
+                if (ch != ' ' and ch != '\n' and ch != '\t') out_i += 1;
+            }
+        }
+    }
+    for (inp[inp_i..]) |ch| {
+        outp[out_i] = ch;
+        if (ch != ' ' and ch != '\n' and ch != '\t') out_i += 1;
+    }
+
+    return outp[0..out_i];
+}
+
 fn despaceAvx512(inp: []const u8, outp: []u8) []u8 {
     return outp[0..despace_avx512(inp.ptr, outp.ptr, inp.len)];
 }
@@ -72,6 +115,14 @@ pub fn main() !void {
         std.mem.doNotOptimizeAway(output_buf);
     }
     const stdlib_time = timer.lap();
+    _ = despaceZigSimd(bytes, output_buf);
+    _ = timer.lap();
+    for (0..iterations) |_| {
+        std.mem.doNotOptimizeAway(despaceZigSimd(bytes, output_buf));
+        std.mem.doNotOptimizeAway(bytes);
+        std.mem.doNotOptimizeAway(output_buf);
+    }
+    const zigavx_time = timer.lap();
     _ = despaceAvx512(bytes, output_buf);
     _ = timer.lap();
     for (0..iterations) |_| {
@@ -91,6 +142,7 @@ pub fn main() !void {
     std.debug.print("string despacing times ({}MB input ran {} times):\n", .{ buffer_size / (1 << 20), iterations });
     std.debug.print("scalar: {}\t\t throughput: {d:.2}GB/s \n", .{ std.fmt.fmtDuration(scalar_time), float(processed_data) / float(scalar_time) });
     std.debug.print("stdlib: {}\t\t throughput: {d:.2}GB/s \n", .{ std.fmt.fmtDuration(stdlib_time), float(processed_data) / float(stdlib_time) });
+    std.debug.print("zigavx: {}\t\t throughput: {d:.2}GB/s \n", .{ std.fmt.fmtDuration(zigavx_time), float(processed_data) / float(zigavx_time) });
     std.debug.print("avx512: {}\t\t throughput: {d:.2}GB/s \n", .{ std.fmt.fmtDuration(avx512_time), float(processed_data) / float(avx512_time) });
     std.debug.print("memcpy: {}\t\t throughput: {d:.2}GB/s \n", .{ std.fmt.fmtDuration(memcpy_time), float(processed_data) / float(memcpy_time) });
 }
